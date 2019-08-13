52ff6a5d21234c3dbf89850290d239efaf7d99b6
[Rayanuc] <[rayhanctg4@gmail.com]>
2019-08-13 11:50:41 +0600

    php7 class list
    php 7 class list added.

---
+++ app/public/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>PHP 7 Tutorial</title>
+
+	<!-- bootstrap css link -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <!-- font-awesome css link -->
+    <link href="assets/css/font-awesome.min.css" rel="stylesheet">
+    <!-- style css link -->
+    <link href="assets/css/style.css" rel="stylesheet">
+    <!-- responsive css link -->
+    <link href="assets/css/responsive.css" rel="stylesheet">
+    <!-- css link end -->
+
+</head>
+<body>
+    <div class="heading">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1><?php echo "All PHP class of PHP 7 Tutorial" ?></h1>
+                </div>
+            </div>
+        </div>
+    </div>
+	<div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="header-menu">
+                    <ul>
+                        <li><a href="class-1/class-1.php" target="_black"><?php echo "Class: 1.1" ?></a></li>
+                        <li><a href="class-2/class-2.php" target="_black"><?php echo "Class: 1.2" ?></a></li>
+                        <li><a href="class-3/class-3.php" target="_black"><?php echo "Class: 1.3" ?></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+	<!-- jquery js link -->
+    <script src="assets/js/jquery.min.js"></script>
+    <!-- bootstrap js link -->
+    <script src="assets/js/bootstrap.min.js"></script>
+    <!-- main js link -->
+    <script src="assets/js/main.js"></script>
+</body>
+</html>
