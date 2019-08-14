cd8a0ca380ea0010c08407f0ee7615d3ecd8b989
[Rayanuc] <[rayhanctg4@gmail.com]>
2019-08-14 23:30:53 +0600

    ১.৪ - পিএইচপিতে আউটপুট প্রিন্ট করা নিয়ে বিস্তারিত
    ১.৪ - পিএইচপিতে আউটপুট প্রিন্ট করা নিয়ে বিস্তারিত

--- app/public/index.php
+++ app/public/index.php
@@ -33,6 +33,7 @@
                         <li><a href="class-1/class-1.php" target="_black"><?php echo "Class: ১.১ - পিএইচপি ডেভেলপমেন্ট এনভায়রনমেন্ট" ?></a></li>
                         <li><a href="class-2/class-2.php" target="_black"><?php echo "Class: ১.২ - ভ্যারিয়েবল পরিচিতি" ?></a></li>
                         <li><a href="class-3/class-3.php" target="_black"><?php echo "Class: ১.৩ - ভ্যারিয়েবল নিয়ে আরও কথা, কনস্ট্যান্ট এবং কমেন্টস" ?></a></li>
+                        <li><a href="class-4/class-4.php" target="_black"><?php echo "Class: ১.৪ - পিএইচপিতে আউটপুট প্রিন্ট করা নিয়ে বিস্তারিত" ?></a></li>
                     </ul>
                 </div>
             </div>
