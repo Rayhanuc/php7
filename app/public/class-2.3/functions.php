db29011e93f94bdb9a471ed0baaeb58d0eb9cb96
[Rayanuc] <[rayhanctg4@gmail.com]>
2019-10-12 01:05:06 +0600

    ২.৩ - ফাংশন প্যারামিটারের ডিফল্ট ভ্যালু বা অপশনাল প্যারামিটার
    ২.৩ - ফাংশন প্যারামিটারের ডিফল্ট ভ্যালু বা অপশনাল প্যারামিটার

---
+++ app/public/class-2.3/functions.php
@@ -0,0 +1,12 @@
+<?php 
+
+
+/*
+
+Determines if the argument is even or odd
+
+*/
+// one parameter can use only
+function serve($foodType= "Coffee",$numberOfItems="1 cup"){// parameter
+	echo "{$numberOfItems} of {$foodType} has/have been served";
+}
